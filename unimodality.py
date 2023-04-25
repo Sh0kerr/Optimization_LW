baea4b4c63dbaa566727f6c82a46dafc551cff66
Sh0kerr <vladko2003@yandex.ru>
2023-04-25 14:36:40 +0300

    Fixed

--- unimodality.py
+++ unimodality.py
@@ -1,56 +1,79 @@
-"""This file relizes the search of unimodality interval ([a, b]) of function f2(x) with a constant step."""
+"""Релизация поиска интервала унимодальности ([a, b]) функции f2(x) с постоянным шагом."""
 
 import matplotlib.pyplot as plt
 from typing import Union, Tuple
 import numpy as np
+import pandas as pd
 
 
 def func(x: float) -> float:
-    """Function that describes f2(x)."""
+    """Задание функции f2(x)"""
     return 2 * (x - 1) ** 2 + (0.01 / (1 - 2 * (x ** 2)))
 
 
-def alg(x: Union[int, float], delta: Union[int, float], a: Union[int, float], b: Union[int, float]) -> Tuple[Union[int, float], Union[int, float], int]:
-    """Function that ."""
-    k = 0
-    x_next= x + delta
-    y_next = func(x_next)
-    y = func(x)
+def step(delta: float) -> float:
+    """Функция шага"""
+    return delta
 
-    while y_next <= y:
-        x_next= x + delta
-        y_next = func(x_next)
-        y = func(x)
-        k += 1
-    
-    a = x
-    b = x_next
-
-    return (a, b, k)
 
 a = 0
 b = 2
-x0, delta = [float(x) for x in input("Print x0 and delta separated by space.\n").split()]
+x0 = 0
+k_limit = 500
+# x0, delta = [float(x) for x in input("Print x0 and delta separated by space.\n").split()]
 
-input_set = np.linspace(a, b, 201)
-output = [func(x) for x in input_set]
+# input_set = np.linspace(a, b, 201)
+# output = [func(x) for x in input_set]
+delta_range = np.linspace(0.001, 0.025, 25)
 
-fx0 = func(x0)
-fx_left = func(x0 - delta)
-fx_right = func(x0 + delta)
-
-if (fx_left >= fx0) and (fx0 >= fx_right):
-    print(alg(x0, delta, a, b))
-elif (fx_left <= fx0) and (func(x0) <= fx_right):
-    delta *= -1
-    print(alg(x0, delta, a, b))
-elif fx_left >= fx0 <= fx_right:
-    a = x0 - delta
-    b = x0 + delta
-    print(a, b)
+b_list = []
+ba_list = []
+k_list = []
 
 
-plt.plot(input_set, output, label="f2(x)")
-plt.legend()
-plt.grid()
-plt.show()
+
+for delta in delta_range:
+    a = []
+    fx0 = func(x0)
+    fx_left = func(x0 - delta)
+    fx_right = func(x0 + delta)
+
+    if fx_left >= fx0 >= fx_right:
+        delta = delta
+    elif fx_left <= fx0 <= fx_right:
+        delta *= -1
+    elif fx_left >= fx0 <= fx_right:
+        exit(0)
+
+    k = 0
+
+    x_next = x0
+    while True:
+        x_prev = x_next
+        x_next += step(delta)
+        if func(x_next) > func(x_prev):
+            a = x_prev
+            b = x_next
+            break
+        else:
+            k += 1
+
+    b_list.append(b)
+    ba_list.append(b-a)
+    k_list.append(k)
+
+
+df = pd.DataFrame()
+df["delta"] = delta_range
+df["x0"] = [2 for _ in range(25)]
+df["b"] = b_list
+df["b - a"] = ba_list
+df["k"] = k_list
+
+print(df)
+
+"""Вывод графика функции""" 
+# plt.plot(input_set, output, label="f2(x)")
+# plt.legend()
+# plt.grid()
+# plt.show()
